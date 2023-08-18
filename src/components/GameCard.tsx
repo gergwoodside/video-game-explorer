aa1bceff923ad2c9185806073f0ae8b121e9f27c
Greg Woodside <greg.woodside@backstopsolutions.com>
2023-08-18 17:11:56 -0500

    display platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
+// bring in the Game object as a prop
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
+    // borderRadius to round out the cards edges
+    // overflow because image was larger than card
+    // so edges were still square, basic CSS
     <Card borderRadius={"10px"} overflow={"hidden"}>
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,21 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Platform {
     id: number;
     name: string;
+    slug: string;
+}
+
+export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: { platform: Platform}[]
+    // parent platforms is not a platform array
+    // it is an array of objects where each object
+    // has a property called platform of type platform.
+    // design smell
   }
   
   
