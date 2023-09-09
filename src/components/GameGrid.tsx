87314e383397ea2ee06cd059cdb36c643e80e6b6
Greg Woodside <greg.woodside@backstopsolutions.com>
2023-09-08 22:49:11 -0500

    create generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContiner from "./GameCardContiner";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
@@ -23,7 +23,7 @@ const GameGrid = () => {
               <GameCardSkeleton key={skeleton} />
             </GameCardContiner>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContiner>
             <GameCard key={game.id} game={game} />
           </GameCardContiner>
