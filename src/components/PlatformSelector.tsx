8c042555b3e6412811b2db45e9d99cb2674b114d
Greg Woodside <greg.woodside@backstopsolutions.com>
2023-09-09 20:21:22 -0500

    add platform sorting

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,26 +1,30 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import React, { useState } from "react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
-  onSelect: () => void;
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-const PlatformSelector = ({ onSelect }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
-  const [selectedPlatform, setSelectedPlatform] = useState("Platforms");
 
   if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platforms
+        {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            key={platform.id}
+            onClick={() => onSelectPlatform(platform)}
+          >
+            {platform.name}
+          </MenuItem>
         ))}
       </MenuList>
     </Menu>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,6 +20,11 @@ export interface Game {
     // design smell
   }
   
-  const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', { params: { genres: selectedGenre?.id }}, [selectedGenre?.id]);
+  const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>('/games', {
+     params: { 
+        genres: selectedGenre?.id, 
+        platforms: selectedPlatform?.id 
+    }}, 
+    [selectedGenre?.id, selectedPlatform?.id]);
   
   export default useGames;
