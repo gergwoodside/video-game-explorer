7923853fe38ef29001595b4db0e207f9ab6ae0a2
Greg Woodside <greg.woodside@backstopsolutions.com>
2023-09-15 14:15:15 -0500

    add search input, create dynamic heading

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '12a00fa08c4447efa276d487e9b81513'
+        key: '02743fc6e6aa4d78bb11c73a56396f91'
     }
 })
